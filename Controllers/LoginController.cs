c5e7a7821dfcb15d1153828e2658f515f9e6cdc2
Eray ALÇİN <erayalcinn@gmail.com>
2022-12-18 04:01:35 +0300

    login page and user profile done.

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -7,6 +7,16 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using wrts.Migrations;
+using System.Diagnostics.Eventing.Reader;
+using Microsoft.AspNetCore.Http;
+using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Security.Principal;
 
 namespace wrts.Controllers
 {
@@ -18,7 +28,7 @@ namespace wrts.Controllers
         {
             return View();
         }
-        [HttpGet]
+        [HttpPost]
 
         public async Task<IActionResult> Check(User loginRequest)
         {
@@ -27,17 +37,47 @@ namespace wrts.Controllers
             var userdepartment = loginRequest.DepartmentID.ToString();
             if (userdetails != null)
             {
-                var claims = new List<Claim>();
-                claims.Add (new Claim(ClaimTypes.Name, userdetails.Name));
-                TempData["Name"] = userdetails.Name;
-                TempData["SName"] = userdetails.Surname;
+                 var dep=db.Department.Find(userdetails.DepartmentID);
 
-                var x = db.Department.Find(userdetails.DepartmentID);
-                TempData["Department"] = x.DepartmentName;
+                var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, userdetails.Name),
+                        new Claim(ClaimTypes.Name, userdetails.Name),
+                        new Claim("Surname", userdetails.Surname),
+                        new Claim("Department",dep.DepartmentName),
+                        //new Claim("FullName", user.FullName),
+                        //new Claim(ClaimTypes.Role, "Administrator"),
+                    };
 
-                var useridentity = new ClaimsIdentity(claims,"Login");
-                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
-                await HttpContext.SignInAsync(principal);
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+                {
+                    //AllowRefresh = <bool>,
+                    // Refreshing the authentication session should be allowed.
+
+                    //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                    // The time at which the authentication ticket expires. A 
+                    // value set here overrides the ExpireTimeSpan option of 
+                    // CookieAuthenticationOptions set with AddCookie.
+
+                    //IsPersistent = true,
+                    // Whether the authentication session is persisted across 
+                    // multiple requests. When used with cookies, controls
+                    // whether the cookie's lifetime is absolute (matching the
+                    // lifetime of the authentication ticket) or session-based.
+
+                    //IssuedUtc = <DateTimeOffset>,
+                    // The time at which the authentication ticket was issued.
+
+                    //RedirectUri = <string>
+                    // The full path or absolute URI to be used as an http 
+                    // redirect response value.
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                                              new ClaimsPrincipal(claimsIdentity),
+                                              authProperties);
                 return RedirectToAction("Index","Home");
             }
             else
